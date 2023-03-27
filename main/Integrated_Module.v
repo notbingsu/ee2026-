a7f4a4b7bd76e0ca0f1e660bc7adf6e961438055
1simjustin <1sim.justin@gmail.com>
2023-03-27 18:14:30 +0800

    added to top

--- main/Integrated_Module.v
+++ main/Integrated_Module.v
@@ -159,6 +159,17 @@ module integrated_grp(
         );
         
     // for menu selection
+    // registers for oled control
+    wire [15:0] oled_state_0;
+    wire [15:0] oled_state_1;
+    wire [15:0] oled_state_2;
+    wire [15:0] oled_state_3;
+    wire [15:0] oled_state_4;
+    wire [15:0] oled_state_5;
+    wire [15:0] oled_state_6;
+    wire [15:0] oled_state_7;
+    wire [15:0] oled_state_8;
+
     always @ (posedge clock) begin
         if (machine_state == 2'b0) begin
             // Centre button
@@ -197,6 +208,8 @@ module integrated_grp(
         end
 	end
 
-    menu menu(.clock(clock), .btnL(btnL), .btnR(btnR), .x(oled_x), .y(oled_y), .oled_data(oled_data), .machine_state(machine_state), .menu_state(menu_state));
+    oled_control control (.machine_state(machine_state), .oled_s0(oled_state_0), .oled_s1(oled_state_1), .oled_s2(oled_state_2), .oled_s3(oled_state_3), .oled_s4(oled_state_4), .oled_s5(oled_state_5), .oled_s6(oled_state_6), .oled_s7(oled_state_7), .oled_s8(oled_state_8), .oled_data(oled_data));
+    menu menu(.clock(clock), .btnL(btnL), .btnR(btnR), .x(oled_x), .y(oled_y), .oled_data(oled_state_0), .machine_state(machine_state), .menu_state(menu_state));
+    gif gif(.clock(clock), .x(oled_x), .y(oled_y), .oled_data(oled_state_7), .machine_state(machine_state));
 
 endmodule
