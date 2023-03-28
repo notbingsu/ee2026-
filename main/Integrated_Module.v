f45b1542eee09ca48ae67eb785798c0e989d5d13
1simjustin <1sim.justin@gmail.com>
2023-03-28 16:22:45 +0800

    pls no issues

--- main/Integrated_Module.v
+++ main/Integrated_Module.v
@@ -30,15 +30,18 @@ module integrated_grp(
     wire [6:0] oled_x, oled_y;
     wire [15:0] oled_data;
     // registers for oled control
+    // unused wires commented out
     wire [15:0] oled_state_0;
-    wire [15:0] oled_state_1;
-    wire [15:0] oled_state_2;
+    // wire [15:0] oled_state_1;
+    // wire [15:0] oled_state_2;
     wire [15:0] oled_state_3;
     wire [15:0] oled_state_4;
     wire [15:0] oled_state_5;
     wire [15:0] oled_state_6;
-    wire [15:0] oled_state_7;
+    // wire [15:0] oled_state_7;
     wire [15:0] oled_state_8;
+    wire [15:0] oled_state_9;
+    wire [15:0] oled_state_10;
     
     // number displayed on oled as selected if applicable
     wire [3:0] num;
@@ -208,8 +211,8 @@ module integrated_grp(
         end
 	end
 
-    oled_control (.machine_state(machine_state), .oled_s0(oled_state_0), .oled_s1(oled_state_1), .oled_s2(oled_state_2), .oled_s3(oled_state_3), .oled_s4(oled_state_4), .oled_s5(oled_state_5), .oled_s6(oled_state_6), .oled_s7(oled_state_7), .oled_s8(oled_state_8), .oled_data(oled_data));
-    menu (.clock(clock), .btnL(btnL), .btnR(btnR), .x(oled_x), .y(oled_y), .oled_data(oled_state_0), .machine_state(machine_state), .menu_state(menu_state));
-    gif (.clock(clock), .x(oled_x), .y(oled_y), .oled_data(oled_state_7), .machine_state(machine_state));
+    oled_control (.machine_state(machine_state), .oled_s0(oled_state_0), .oled_s3(oled_state_3), .oled_s4(oled_state_4), .oled_s5(oled_state_5), .oled_s6(oled_state_6), .oled_s8(oled_state_8), .oled_s9(oled_state_9), .oled_s10(oled_state_10), .oled_data(oled_data));
+    menu (.clock(clock), .btnL(btnL), .btnR(btnR), .btnU(btnU), .btnD(btnD), .x(oled_x), .y(oled_y), .oled_data(oled_state_0), .machine_state(machine_state), .menu_state(menu_state));
+    gif (.clock(clock), .x(oled_x), .y(oled_y), .oled_data(oled_state_9), .machine_state(machine_state));
 
 endmodule
--- main/oled_control.v
+++ main/oled_control.v
@@ -2,22 +2,24 @@
 
 module oled_control(
     input [3:0] machine_state,
-    input [15:0] oled_s0, oled_s1, oled_s2, oled_s3, oled_s4, oled_s5, oled_s6, oled_s7, oled_s8,
+    input [15:0] oled_s0, oled_s3, oled_s4, oled_s5, oled_s6, oled_s8, oled_s9, oled_s10,
     output reg [15:0] oled_data
     );
 
     always @ (*) begin
-        // update oled states where neccessary
         case (machine_state)
             4'b0000: oled_data = oled_s0;
-            4'b0001: oled_data = 0;
-            4'b0010: oled_data = 0;
-            4'b0011: oled_data = 0;
-            4'b0100: oled_data = 0;
-            4'b0101: oled_data = 0;
-            4'b0110: oled_data = 0;
-            4'b0111: oled_data = oled_s7;
-            4'b1000: oled_data = 0;
+            // 4'b0001: oled_data = 0;
+            // 4'b0010: oled_data = 0;
+            4'b0011: oled_data = oled_s3;
+            4'b0100: oled_data = oled_s4;
+            4'b0101: oled_data = oled_s5;
+            4'b0110: oled_data = oled_s6;
+            // 4'b0111: oled_data = 0;
+            4'b1000: oled_data = oled_s8;
+            4'b1001: oled_data = oled_s9;
+            4'b1010: oled_data = oled_s10;
+            default: oled_data = 0;
         endcase
     end
 endmodule
